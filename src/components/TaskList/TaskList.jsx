2c922c88538164a1fc909716a5a8ddfcc051e95b
a-nester <a_nester@ukr.net>
2024-06-15 00:21:39 +0300

    +

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,12 +1,28 @@
-import { Task } from '../Task/Task';
-import css from './TaskList.module.css';
+import { useSelector } from "react-redux";
+import { Task } from "../Task/Task";
+import css from "./TaskList.module.css";
+import { statusFilters } from "../../redux/constants";
 
 export const TaskList = () => {
-  const tasks = [];
+  const getVisibleTasks = (tasks, statusFilter) => {
+    switch (statusFilter) {
+      case statusFilters.active:
+        return tasks.filter((task) => !task.completed);
+      case statusFilters.completed:
+        return tasks.filter((task) => task.completed);
+      default:
+        return tasks;
+    }
+  };
 
+  const tasks = useSelector((state) => state.tasks);
+
+  const statusFilter = useSelector((state) => state.filters.status);
+
+  const visibleTasks = getVisibleTasks(tasks, statusFilter);
   return (
     <ul className={css.list}>
-      {tasks.map((task) => (
+      {visibleTasks.map((task) => (
         <li className={css.listItem} key={task.id}>
           <Task task={task} />
         </li>
